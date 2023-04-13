89b124104bd4246af208b19e0c5a4de8f6573fc9
BharathBhagath <90191821+BharathBhagath@users.noreply.github.com>
2023-04-13 10:31:23 +0530

    Update webhooks-list.sh

--- webhooks-list.sh
+++ webhooks-list.sh
@@ -3,7 +3,7 @@
 # Set variables for the repository and GitHub API token
 REPO_OWNER=BharathBhagath
 REPO_NAME=hello-world
-GITHUB_TOKEN="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"
+GITHUB_TOKEN=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
 # Make the API call to list webhooks
 curl -H "Authorization: token $GITHUB_TOKEN" \
     https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/hooks
